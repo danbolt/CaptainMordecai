bf03aae503ae9b72a26b6c0931599ea13186d6c6
Daniel Savage <dansvg@gmail.com>
2015-02-10 23:54:16 -0800

    Added stylesheets and Phaser library

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,61 @@
-/* Gulp and processing plugins */
+/* Hello, entusiastic code-reader! The following gulpfile compiles coffeescript,
+ * and places it in the build folder, along with minified game assets.
+ * Feel free to shoot Daniel a question if you're curious about what is
+ * here.
+ */
+
+/* Gulp and processing plugins. */
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var sass = require('gulp-sass');
 var changed = require('gulp-changed');
 var imagemin = require('gulp-imagemin');
 var minifyHTML = require('gulp-minify-html');
 
-gulp.task('coffee', function() {
-  gulp.src('./src/script/*.coffee')
+/* Source and build destination directories for files. */
+var coffeeSrc = './src/script/*.coffee';
+var coffeeDst = './build/script';
+var libSrc = './src/lib/*.js'
+var libDst = './build/lib'
+var imgSrc = './src/img/*';
+var imgDst = './build/img';
+var htmlSrc = './src/*.html';
+var htmlDst = './build/';
+var styleSrc = './src/scss/*.scss';
+var styleDst = './build/css';
+
+gulp.task('coffee', function()
+{
+  gulp.src(coffeeSrc)
     .pipe(coffee( { bare: true} ).on('error', gutil.log))
-    .pipe(gulp.dest('./build/script'));
+    .pipe(gulp.dest(coffeeDst));
 });
 
-gulp.task('img', function() {
-  var imgSrc = './src/img/*';
-  var imgDst = './build/img';
+gulp.task('lib', function()
+{
+  gulp.src(libSrc)
+  .pipe(gulp.dest(libDst));
+});
 
+gulp.task('img', function()
+{
   gulp.src(imgSrc)
   .pipe(gulp.dest(imgDst));
 });
 
-gulp.task('htmlpage', function() {
-  var htmlSrc = './src/*.html';
-  var htmlDst = './build/';
-
+gulp.task('htmlpage', function()
+{
   gulp.src(htmlSrc)
   .pipe(minifyHTML())
   .pipe(gulp.dest(htmlDst));
 });
 
-gulp.task('default', ['coffee', 'img', 'htmlpage'], function() {});
+gulp.task('style', function()
+{
+  gulp.src(styleSrc)
+  .pipe(sass())
+  .pipe(gulp.dest(styleDst));
+});
+
+gulp.task('default', ['coffee', 'lib', 'img', 'htmlpage', 'style'], function() {});
--- src/script/main.coffee
+++ src/script/main.coffee
@@ -1,1 +1,2 @@
-console.log 'hello world'
+main = () ->
+  console.log Phaser
